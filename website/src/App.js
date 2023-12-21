a294db04fb27e52d828121edf37f26b1ec07e307
Kevin Danis <kjdanis@gmail.com>
2023-12-21 10:36:13 -0500

    Initial

---
+++ website/src/App.js
@@ -0,0 +1,14 @@
+import logo from "./logo.svg";
+
+function App() {
+  return (
+    <div className="flex justify-center">
+      <header className="">
+        <img src={logo} className="" alt="logo" />
+        <p className="font-bold text-2xl">Kevin Danis</p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
