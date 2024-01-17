e6e68db52c84606aabe1201503b69245356de3f9
Kevin Danis <kjdanis@gmail.com>
2024-01-17 18:31:26 -0500

    Testing Affilate Link

--- website/src/routes/about.jsx
+++ website/src/routes/about.jsx
@@ -4,6 +4,7 @@ const About = () => {
     return (
         <div>
             <h1>About</h1>
+            <a target="_blank" href="https://www.amazon.com/b?_encoding=UTF8&tag=kjdii-20&linkCode=ur2&linkId=29ab19e698d285fbc261a7973ca1445f&camp=1789&creative=9325&node=133140011">Kindle Best Sellers</a>
             
         </div>
     );
